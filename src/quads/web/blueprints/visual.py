6f38aa069c291ec273e0b8917af8f50eb0fc2aac
Thirumalesh Aaraveti <athiruma@redhat.com>
2024-10-02 00:53:49 +0530

    Add dynamic menu bar and nav using flask_nav3
    Change-Id: Id6e1156ff2e8c33dfda1869e84f44913df394ca6

--- src/quads/web/blueprints/visual.py
+++ src/quads/web/blueprints/visual.py
@@ -1,7 +1,11 @@
+import asyncio
 import os
 
 from flask import Blueprint, abort, render_template
 
+from quads.config import Config
+from quads.tools.external.foreman import Foreman
+from quads.quads_api import QuadsApi as Quads
 from quads.web.blueprints.common import WEB_CONTENT_PATH, get_file_paths
 
 TEMPLATE_DIR = os.path.join(WEB_CONTENT_PATH, "visual")
@@ -9,6 +13,15 @@ visual_bp = Blueprint(
     "visual",
     __name__,
     template_folder=TEMPLATE_DIR,
+)
+
+quads = Quads(Config)
+loop = asyncio.new_event_loop()
+foreman = Foreman(
+    Config["foreman_api_url"],
+    Config["foreman_username"],
+    Config["foreman_password"],
+    loop=loop,
 )
 
 
