f975f19282aad1192ddaaa2d9ecb18697eb31ca0
Gonzalo Rafuls <grafuls@redhat.com>
2024-10-31 21:17:06 +0100

    fix: lshw, templates, conf var removal
    * fix lshw not working
    * fix lshw2meta interface update
    * fix wiki_url conf removal
    * fix tests for templates for new url
    
    closes: https://github.com/redhat-performance/quads/issues/542
    closes: https://github.com/redhat-performance/quads/issues/543
    closes: https://github.com/redhat-performance/quads/issues/544
    
    Change-Id: Ia0155091c2ade7de9f86fc47a71cb9ac8a4f1173

--- src/quads/tools/lshw.py
+++ src/quads/tools/lshw.py
@@ -21,9 +21,9 @@ def run_lshw(hostname: str, file_path: str) -> None:
     except Exception:
         print(f"Something went wrong trying to connect to: {hostname}")
         return
-    _, output = ssh_helper.run_cmd("lshw -xml")
+    _, output = ssh_helper.run_cmd("lshw -json")
     if output:
-        with open(file_path, "w") as _file:
+        with open(file_path, "w+") as _file:
             for line in output:
                 _file.writelines(line)
 
@@ -36,13 +36,13 @@ def main() -> None:
     cloud = quads.get_cloud("cloud01")
     hosts = quads.filter_hosts({"cloud": cloud.name, "retired": False, "broken": False})
     for host in hosts:
-        file_name = f"{host.name}.xml"
+        file_name = f"{host.name}.json"
         file_path = os.path.join(LSHW_OUTPUT_DIR, file_name)
         if os.path.exists(file_path):
             if os.path.getsize(file_path) < 1:
                 run_lshw(host.name, file_path)
-            else:
-                run_lshw(host.name, file_path)
+        else:
+            run_lshw(host.name, file_path)
 
 
 if __name__ == "__main__":  # pragma: no cover
