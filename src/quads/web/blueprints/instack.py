f7634e0f90b3f74c63e1227adcac6942ad8398f5
Gonzalo Rafuls <grafuls@redhat.com>
2024-11-14 19:54:46 +0100

    fix: cached instack files & navbar undescore
    Change-Id: I3b6ee7de8a16aae2f2e51d6fdbc7bf66003c0fef

--- src/quads/web/blueprints/instack.py
+++ src/quads/web/blueprints/instack.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Blueprint, abort, render_template
+from flask import Blueprint, abort, make_response, render_template, send_from_directory
 
 from quads.web.blueprints.common import WEB_CONTENT_PATH, get_file_paths
 
@@ -12,11 +12,19 @@ instack_bp = Blueprint(
 )
 
 
-@instack_bp.route("/<cloud>")
-def instack(cloud):
+@instack_bp.route("/<file>")
+def instack(file):
     path = os.path.join(WEB_CONTENT_PATH, "instack")
-    file_paths = get_file_paths(path)
-    for file in file_paths:
-        if cloud in file:
-            return render_template(file)
-    return abort(404)
+    file_path = os.path.join(path, file)
+    if not os.path.exists(file_path):
+        return abort(404)
+    
+    with open(file_path, 'r') as f:
+        content = f.read()
+    
+    response = make_response(content)
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
