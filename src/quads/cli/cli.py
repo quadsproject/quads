0f35f328d53df6d05435795d2e286a3c34687c13
Thirumalesh Aaraveti <athiruma@redhat.com>
2024-11-28 11:00:40 +0530

    Added the ls-notification, mod-notification #383
    Change-Id: Id6e1fda7c93cca37112c470433cf82b20b6a23f1

--- src/quads/cli/cli.py
+++ src/quads/cli/cli.py
@@ -1914,3 +1914,75 @@ class QuadsCli:
 
         validate_env(_args, _loop, self.logger)
         self.logger.info("Quads assignments validation executed.")
+
+    def action_list_notifications(self):
+        cloud_name = self.cli_args.get("cloud")
+        try:
+            if not cloud_name:
+                assignments = self.quads.get_active_assignments()
+            else:
+                assignment = self.quads.get_active_cloud_assignment(cloud_name=cloud_name)
+                assignments = [] if not assignment else [assignment]
+        except (APIServerException, APIBadRequest) as ex:
+            raise CliException(str(ex))
+        headers = ["fail", "success", "initial", "pre_initial", "pre", "one_day", "three_days", "five_days",
+                   "seven_days"]
+        rows = []
+        if assignments:
+            for ass in assignments:
+                cloud_name = str(ass.cloud.name)
+                ticket = str(ass.ticket)
+                notification = ass.notification
+                row = [cloud_name, ticket]
+                for header in headers:
+                    row.append(str(getattr(notification, header)))
+                rows.append(row)
+            headers.insert(0, "cloud")
+            headers.insert(1, "ticket")
+
+            col_widths = [max(len(str(item)) for item in col) for col in zip(headers, *rows)]
+
+            def format_row(data_row):
+                return "  ".join(str(item).ljust(width) for item, width in zip(data_row, col_widths))
+
+            table = [format_row(headers), "=" * (sum(col_widths) + 2 * (len(headers) - 1))]
+            table.extend(format_row(row) for row in rows)
+            self.logger.info("\n".join(table))
+        else:
+            message = "WARNING: there are no current or future schedules"
+            if not cloud_name:
+                self.logger.warning(f"{message}")
+            else:
+                self.logger.warning(f"{message} for {cloud_name}")
+
+    def action_modify_notification(self):
+        cloud_name = self.cli_args.get("cloud")
+        if not cloud_name:
+            raise CliException("Missing parameter --cloud")
+        mod_notification_arg_names = [
+            "fail", "success", "initial", "pre_initial",
+            "pre", "one_day", "three_days", "five_days", "seven_days"]
+        modify_notifications = list(filter(lambda item: self.cli_args.get(item), mod_notification_arg_names))
+        if not modify_notifications:
+            err_message = ""
+            for arg in mod_notification_arg_names:
+                err_message += f"\n--{arg.replace('_', '-')} [true|false]"
+            raise CliException("At least one arg should be given: {}".format(err_message))
+        try:
+            assignment = self.quads.get_active_cloud_assignment(cloud_name=cloud_name)
+        except (APIServerException, APIBadRequest) as ex:
+            raise CliException(str(ex))
+        if assignment:
+            assignment_id = assignment.id
+            payload = {}
+            for arg in modify_notifications:
+                payload.update({arg: self.cli_args.get(arg)})
+            response = self.quads.update_notification(notification_id=assignment_id, data=payload)
+            if response.status_code == 200:
+                self.logger.info(f"{cloud_name}, Notification updated successfully\n")
+                self.action_list_notifications()
+            else:
+                self.logger.error("Something went wrong while updating notification")
+        else:
+            self.logger.warning(f"{cloud_name}, No active cloud assignment found")
+
