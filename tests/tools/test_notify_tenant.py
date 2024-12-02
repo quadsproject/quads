14b90cc42630761e6cce831f3af72b6f36ccadb3
Kambiz Aghaiepour <kambiz@redhat.com>
2024-12-02 10:01:01 -0500

    General purpose tenant contact tool
    To use this contact tool from the tools directory, you will
    need to create a message file, e.g. /tmp/message, with content
    example:
    
      --- snip ---
    
    Regarding your allocation
    
    Description: {{description}}
    Allocation: {{cloud_name}}
    Ticket: {{ticket}}
    
    This message was composed using a simple jinja template.  The available variables are:
    
        description (taken from allocation object)
        cloud_name (taken from the cloud name)
        ticket (taken from allocation object)
    
    This is just a test message while testing the notification tool.
    
      --- snip ---
    
    and then call the tool with one of the following forms:
    
    (rack based)
       ./notify_tenant.py --message /tmp/message  --subject "testing - outage impacting racks F18 and F22" --rack "f18 e22"
    
    (allocation based)
        ./notify_tenant.py --message /tmp/message  --subject "testing - outage in CLOUD02 CLOUD03'" --cloud 'cloud02 cloud03'
    
    (all allocations)
        ./notify_tenant.py --message /tmp/message  --subject "testing - ALL environments notification" --all
    
    Fixes: https://github.com/redhat-performance/quads/issues/538
    Change-Id: I58a7b076b019db05925885b41f57b3f8a4a17078

---
+++ tests/tools/test_notify_tenant.py
@@ -0,0 +1,94 @@
+import asyncio
+import tempfile
+import pytest
+import os
+from unittest.mock import patch, AsyncMock, MagicMock
+from tests.tools.test_base import TestBase
+
+from quads.tools.notify_tenant import (
+    verify_argparse as notify_tenant_verify_argparse,
+    determine_action as notify_tenant_determine_action,
+    send_message as notify_tenant_send_message,
+    post_message as notify_tenant_post_message,
+)
+
+
+class FakeArgs(object):
+    def __init__(self, subject, message, rack, cloud, all):
+        self.subject = subject
+        self.message = message
+        self.rack = rack
+        self.cloud = cloud
+        self.all = False
+
+
+class TestNotifyTenant(TestBase):
+
+    @pytest.fixture(scope="class", autouse=True)
+    def setup(self, request):
+        content = "Hello world."
+        f = tempfile.NamedTemporaryFile(delete=False)
+        with open(f.name, "w") as fp:
+            fp.write(content)
+
+        fake_args = FakeArgs("Test Subject", f.name, None, "cloud01", False)
+
+        yield content, fake_args
+
+        # remove tempfile
+        os.remove(f.name)
+
+    @patch("quads.tools.notify_tenant.Postman")
+    def test_notify_tenant_send_message(self, mock_postman, setup):
+        owner = "quads_user"
+        cc = ["cc1", "cc2"]
+        ticket = "12345"
+        description = "cloud description"
+        cloud_name = "cloud01"
+        content, fake_args = setup
+
+        # Call the function
+        notify_tenant_send_message(fake_args, owner, cc, ticket, description, cloud_name)
+
+        # Assert that Postman was called with the correct arguments
+        mock_postman.assert_called_once_with(
+            "INFO: [%s] %s" % (cloud_name, fake_args.subject),
+            owner,
+            [
+                "someuser@example.com",
+                "someuser@example.com",
+                "someuser@example.com",
+                "someuser@example.com",
+                "cc1@example.com",
+                "cc2@example.com",
+            ],
+            content,
+        )
+
+    @patch("quads.tools.external.jira.aiohttp.ClientSession.post")
+    @patch("quads.tools.notify_tenant.QuadsApi.filter_assignments")
+    @pytest.mark.asyncio
+    def test_notify_tenant_post_message(self, mock_ass, mock_post, setup):
+        resp = AsyncMock()
+        resp.json.return_value = {}
+        resp.status = 200
+        mock_post.return_value.__aenter__.return_value = resp
+        ticket = "12345"
+        mock_ass.return_value = MagicMock(ticket=ticket, status=200)
+        description = "cloud description"
+        cloud_name = "cloud01"
+        _, fake_args = setup
+        result = notify_tenant_post_message(fake_args, ticket, description, cloud_name)
+        assert result is True
+
+    @patch("quads.tools.notify_tenant.QuadsApi")
+    def test_notify_tenant_determine_action(self, mock_api, caplog, setup):
+        _, fake_args = setup
+        mock_api.get_cloud.return_value = MagicMock()
+        mock_api.get_clouds.return_value = MagicMock()
+        mock_api.filter_hosts.return_value = MagicMock()
+        mock_api.filter_assignments.return_value = MagicMock()
+        results = notify_tenant_determine_action(fake_args)
+
+        assert isinstance(results, list)
+        assert "Skipping notification for cloud01. This is used for available hosts." in caplog.text
