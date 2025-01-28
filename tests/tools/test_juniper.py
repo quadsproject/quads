95d5437d1c18828e3cae46c9186bcafe23b42b62
Kambiz Aghaiepour <kambiz@redhat.com>
2025-01-28 08:11:51 -0500

    Set port speed under interfaces
    This patch covers a fix on juniper connected interfaces
    to explicitly set the juniper connected port speed to the
    speed defined in the quads database for the interface(s)
    of a host.
    
    If the interface speed is set to 1, then there
    will be no additional setting.  Otherwise, speed is looked
    up and set accordingly. For example, if the speed is set
    to 25 in the associated quads data for the interface, then
    the switch will get the additional:
    
       set interfaces <interface name> speed 25g
    
    Fixes: https://github.com/redhat-performance/quads/issues/566
    Change-Id: I7c2d3788bb495eea6d08dbd43c4451e01c570823

--- tests/tools/test_juniper.py
+++ tests/tools/test_juniper.py
@@ -10,17 +10,18 @@ class TestJuniper(object):
 
     ip_address = "10.0.0.6"
     switch_port = "10.0.0.2"
+    port_speed = "1"
     old_vlan = "10.0.0.3"
     new_vlan = "10.0.0.4"
 
     def test_object_parameters(self):
-        juniper = Juniper(self.ip_address, self.switch_port, self.old_vlan, self.new_vlan)
+        juniper = Juniper(self.ip_address, self.switch_port, self.port_speed, self.old_vlan, self.new_vlan)
         assert juniper.ip_address == self.ip_address
 
     @patch("quads.tools.external.juniper.pexpect.spawn")
     def test_close(self, mock_spawn):
         mock_spawn.return_value.__aenter__.return_value = Mock()
-        juniper = Juniper(self.ip_address, self.switch_port, self.old_vlan, self.new_vlan)
+        juniper = Juniper(self.ip_address, self.switch_port, self.port_speed, self.old_vlan, self.new_vlan)
         juniper.connect()
         juniper.close()
         juniper.child = None
@@ -29,14 +30,14 @@ class TestJuniper(object):
     @patch("quads.tools.external.juniper.pexpect.spawn")
     def test_connect(self, mock_spawn):
         mock_spawn.return_value.__aenter__.return_value = Mock()
-        juniper = Juniper(self.ip_address, self.switch_port, self.old_vlan, self.new_vlan)
+        juniper = Juniper(self.ip_address, self.switch_port, self.port_speed, self.old_vlan, self.new_vlan)
         juniper.connect()
         assert juniper.child
 
     @patch("quads.tools.external.juniper.pexpect.spawn")
     def test_connect_exception(self, mock_spawn):
         mock_spawn.side_effect = pexpect.exceptions.TIMEOUT("Timeout")
-        juniper = Juniper(self.ip_address, self.switch_port, self.old_vlan, self.new_vlan)
+        juniper = Juniper(self.ip_address, self.switch_port, self.port_speed, self.old_vlan, self.new_vlan)
         with pytest.raises(JuniperException):
             juniper.connect()
             assert not juniper.child
@@ -44,7 +45,7 @@ class TestJuniper(object):
     @patch("quads.tools.external.juniper.pexpect.spawn")
     def test_execute(self, mock_spawn):
         mock_spawn.return_value.__aenter__.return_value = Mock()
-        juniper = Juniper(self.ip_address, self.switch_port, self.old_vlan, self.new_vlan)
+        juniper = Juniper(self.ip_address, self.switch_port, self.port_speed, self.old_vlan, self.new_vlan)
         juniper.connect()
         juniper.execute(command="ls")
         assert juniper.child
@@ -52,21 +53,21 @@ class TestJuniper(object):
     @patch("quads.tools.external.juniper.pexpect.spawn")
     def test_set_port(self, mock_spawn):
         mock_spawn.side_effect = Mock()
-        juniper = Juniper(self.ip_address, self.switch_port, None, self.new_vlan)
+        juniper = Juniper(self.ip_address, self.switch_port, self.port_speed, None, self.new_vlan)
         response = juniper.set_port()
         assert response
 
     @patch("quads.tools.external.juniper.pexpect.spawn")
     def test_set_port_exception(self, mock_spawn):
         mock_spawn.side_effect = pexpect.exceptions.TIMEOUT("Timeout")
-        juniper = Juniper(self.ip_address, self.switch_port, self.old_vlan, self.new_vlan)
+        juniper = Juniper(self.ip_address, self.switch_port, self.port_speed, self.old_vlan, self.new_vlan)
         response = juniper.set_port()
         assert not response
 
     @patch("quads.tools.external.juniper.pexpect.spawn")
     def test_set_port_old_vlan(self, mock_spawn):
         mock_spawn.side_effect = Mock()
-        juniper = Juniper(self.ip_address, self.switch_port, self.old_vlan, self.new_vlan)
+        juniper = Juniper(self.ip_address, self.switch_port, self.port_speed, self.old_vlan, self.new_vlan)
         response = juniper.set_port()
         assert response
 
@@ -76,6 +77,7 @@ class TestJuniper(object):
         juniper = Juniper(
             ip_address=self.ip_address,
             switch_port=self.switch_port,
+            port_speed=self.port_speed,
             old_vlan=None,
             new_vlan=self.new_vlan,
         )
@@ -85,6 +87,6 @@ class TestJuniper(object):
     @patch("quads.tools.external.juniper.pexpect.spawn")
     def test_convert_port_public_exception(self, mock_spawn):
         mock_spawn.side_effect = pexpect.exceptions.TIMEOUT("Timeout")
-        juniper = Juniper(self.ip_address, self.switch_port, self.old_vlan, self.new_vlan)
+        juniper = Juniper(self.ip_address, self.switch_port, self.port_speed, self.old_vlan, self.new_vlan)
         response = juniper.convert_port_public()
         assert not response
