81ea71325a8a41aeb2599b9e4506212a0fcb88e2
Sebastian Jug <seb@stianj.ug>
2025-03-31 11:32:47 -0400

    Use bootstrap dark for dark theme, svg for toggle icon
    fixes: https://github.com/redhat-performance/quads/issues/529
    
    Change-Id: Iffe290f9770cb537f0ae6b43e66d28ed434e5e12

--- src/quads/web/static/js/main.js
+++ src/quads/web/static/js/main.js
@@ -17,3 +17,27 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Theme Toggler Logic
+document.addEventListener('DOMContentLoaded', (event) => {
+    const themeToggleBtn = document.getElementById('themeToggleBtn');
+    const htmlElement = document.documentElement;
+
+    // Apply the saved theme on initial load
+    let currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light'; // Default to light
+    htmlElement.setAttribute('data-bs-theme', currentTheme);
+
+    // Add event listener for the theme toggle button
+    themeToggleBtn.addEventListener('click', function() { // Listen on the button
+        let theme = htmlElement.getAttribute('data-bs-theme');
+
+        // Toggle theme
+        if (theme === 'dark') {
+            htmlElement.setAttribute('data-bs-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        } else {
+            htmlElement.setAttribute('data-bs-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    });
+});
