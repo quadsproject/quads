7db0ddadcafce20b65b1d1008fabccafb6fee6a5
Gonzalo Rafuls <grafuls@redhat.com>
2025-05-15 09:47:40 +0200

    M&R refactor
    Moved ls-switch-conf, mod-switch-conf, verify-switch-conf to quads-cli
    
    Change-Id: I9a9d72f226ca7acea818935006b4e36f1845db92

--- tests/tools/test_modify_switch_conf.py
+++ tests/tools/test_modify_switch_conf.py
@@ -3,18 +3,21 @@ import pytest
 from quads.quads_api import APIBadRequest
 from quads.server.dao.cloud import CloudDao
 from quads.server.dao.host import HostDao
-from quads.tools.modify_switch_conf import verify as modify_switch_verify
+from quads.tools import logger
+from quads.tools.external.switch import Switch
 
 
 def test_verify_false():
     with pytest.raises(APIBadRequest):
-        modify_switch_verify("host.verify.example.com.example.com")
+        switch = Switch(logger)
+        switch.verify("host.verify.example.com.example.com")
 
 
 def test_verify_empty():
     CloudDao.create_cloud("cloudverify")
     HostDao.create_host("host.verify.example.com", "r640", "unittest", "cloudverify")
-    response = modify_switch_verify("host.verify.example.com")
+    switch = Switch(logger)
+    response = switch.verify("host.verify.example.com")
     assert not response
     HostDao.remove_host("host.verify.example.com")
     CloudDao.remove_cloud("cloudverify")
