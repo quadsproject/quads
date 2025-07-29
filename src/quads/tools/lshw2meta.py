8a5c386e41c2b7ec7b96d191ddf44a7c398934ae
QuantumPosix <cenright@redhat.com>
2025-07-29 06:53:43 -0400

    feat: Removed unused drivers, added in more controller types fixes: https://github.com/redhat-performance/quads/issues/620
    Change-Id: Idd44ad8064f4b2c30698ebe41cf4240729934549

--- src/quads/tools/lshw2meta.py
+++ src/quads/tools/lshw2meta.py
@@ -138,9 +138,17 @@ for _d, _, _files in os.walk(MD_DIR):  # pragma: no cover
                             )
                             print(f"  Created CPU processor: {data}")
                     # GPU
+                    recognized_gpu_drivers = {"nouveau"}
+                    recognized_gpu_controllers = {"3D controller", "Display controller"}
                     for child in [child for child in children if child.value["class"] == "display"]:
                         configuration = child.value.get("configuration")
-                        if configuration.get("driver") == "nouveau":
+                        description = child.value.get("description")
+                        driver = configuration.get("driver") if configuration else None
+
+                        driver_recognized = driver in recognized_gpu_drivers
+                        is_3d_controller = description in recognized_gpu_controllers
+
+                        if driver_recognized or is_3d_controller:
                             data = {
                                 "handle": child.value.get("handle"),
                                 "vendor": child.value.get("vendor"),
